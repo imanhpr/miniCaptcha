6805fb444acc7554349790ba0e607aadfce1d897
imanhpr <imanhpr1999@gmail.com>
2020-05-03 16:46:07 +0430

    version updated.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="miniCaptcha",
-    version="1.0.0",
+    version="1.0.1",
     description="Create Simple Image Captcha For Normal Use",
     long_description=README,
     long_description_content_type="text/markdown",
